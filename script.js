64da7cade988afd165e2bf245195e194a07fa465
samuelmideksa <113627346+samuelmideksa@users.noreply.github.com>
2024-07-12 19:56:09 +0300

    base logic implemented

---
+++ script.js
@@ -0,0 +1,20 @@
+let firstCard = 3
+let secondCard = 10
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let message = ""
+let isAlive = true
+
+if (sum < 21) {
+    message = "Do you want to draw a new card? 🙂"
+}
+else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳"
+    hasBlackJack = true
+}
+else {
+    message = "You're out of the game! 😭"
+    isAlive = false
+}
+
+console.log(message)
