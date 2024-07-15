80df1c4546247d2952a572e36089829dba8a4ff2
samuelmideksa <113627346+samuelmideksa@users.noreply.github.com>
2024-07-15 10:51:37 +0300

    first-commit

--- script.js
+++ script.js
@@ -4,17 +4,30 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let message = ""
 let isAlive = true
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-if (sum < 21) {
-    message = "Do you want to draw a new card? 🙂"
-}
-else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-}
-else {
-    message = "You're out of the game! 😭"
-    isAlive = false
+function startGame() {
+    cardsEl.textContent = "Cards: " + firstCard + ", " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    if (sum < 21) {
+        message = "Do you want to draw a new card?"
+    }
+    else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    }
+    else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+
+    messageEl.textContent = message
 }
 
-console.log(message)
+function newCard() {
+    let newCard = 7
+    sum += newCard
+    startGame()
+}
