dad813e966aad6c1ed183206570db8f23d3706cb
samuelmideksa <113627346+samuelmideksa@users.noreply.github.com>
2024-07-26 17:10:09 +0300

    use array to store cards

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let firstCard = 3
 let secondCard = 10
+let cards = [3, 10]
 let sum = firstCard + secondCard
 let hasBlackJack = false
-let message = ""
 let isAlive = true
+let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
@@ -13,7 +14,11 @@ function startGame() {
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -30,5 +35,6 @@ function renderGame() {
 function newCard() {
     let newCard = 8
     sum += newCard
+    cards.push(newCard)
     renderGame()
 }
